eac10a29a539e5c00157caaceb7295b23443713a
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-03-30 16:40:50 +0200

    Add style to interactive states of the Button component

--- app/components/Button/buttonStyles.js
+++ app/components/Button/buttonStyles.js
@@ -25,6 +25,20 @@ const buttonStyles = css`
   overflow: visible;
   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
     border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:hover {
+    color: #fff;
+    background-color: #0069d9;
+    border-color: #0062cc;
+    text-decoration: none;
+  }
+
+  &:focus {
+    color: #fff;
+    background-color: #0069d9;
+    border-color: #0062cc;
+    box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
+  }
 `;
 
 export default buttonStyles;
