fe0d4845c5fd84f69f33c09fc6dcd0519e2c60cf
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-03-30 16:56:32 +0200

    Add responsive rules to the contaner element

--- app/global-styles.js
+++ app/global-styles.js
@@ -27,9 +27,29 @@ const GlobalStyle = createGlobalStyle`
   }
 
   .container {
-    max-width: 1140px;
+    max-width: none;
     margin: 0 auto;
     width: 100%;
+
+    @media (max-width: 576px) {
+      padding: 0 15px;
+    }
+
+    @media (min-width: 576px) {
+      max-width: 540px;
+    }
+
+    @media (min-width: 768px) {
+      max-width: 720px;
+    }
+
+    @media (min-width: 992px) {
+      max-width: 960px;
+    }
+
+    @media (min-width: 1200px)
+      max-width: 1140px;
+    }
   }
 `;
 
