c2f46f070b6fe6e0ca921d9a4a93d89d02334646
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-03-30 16:38:08 +0200

    Improve NavBar style and alignment

--- app/components/Header/Container.js
+++ app/components/Header/Container.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.div`
+  align-items: center;
   display: flex;
   justify-content: space-between;
+  padding: 1rem 0;
 `;
--- app/components/Header/NavBar.js
+++ app/components/Header/NavBar.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.nav`
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(255, 255, 255, 0));
   position: absolute;
   top: 0;
   width: 100%;
