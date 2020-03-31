b60edad3b693d6aa3c8b4c89a173af874216aabd
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-03-31 19:36:14 +0200

    Responsiveness improvements on the Store component

--- app/components/Store/H3.js
+++ app/components/Store/H3.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 const H3 = styled.h3`
-  margin: 0 0 0.7rem;
+  margin: 0 0 0.5rem;
   font-size: 0.7rem;
   font-weight: 600;
 
   @media (min-width: 576px) {
     font-size: 0.95rem;
+    margin: 0 0 0.7rem;
   }
 `;
 
--- app/components/Store/P.js
+++ app/components/Store/P.js
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 const P = styled.p`
   margin: 0 0 0.2rem;
-  font-size: 0.8rem;
+  font-size: 0.6rem;
   font-weight: 400;
+
+  @media (min-width: 576px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export default P;
