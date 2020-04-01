3e68107060f0a1cb95f30bb0221a32906f54092f
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-01 19:08:58 +0200

    Move Firebase credentials to .env file

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -8,14 +8,14 @@ import { storesLoaded, storesLoadingError } from './actions';
 import { LOAD_STORES } from './constants';
 
 const firebaseConfig = {
-  apiKey: 'xxxxxxxxx',
-  authDomain: 'xxxxxxxxx',
-  databaseURL: 'xxxxxxxxx',
-  projectId: 'xxxxxxxxx',
-  storageBucket: 'xxxxxxxxx',
-  messagingSenderId: 'xxxxxxxxx',
-  appId: 'xxxxxxxxx',
-  measurementId: 'xxxxxxxxx',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATA_BASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 export function* getStores() {
