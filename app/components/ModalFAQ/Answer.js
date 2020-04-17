8244cbbf17db0e1a5f0a7ae5aa80a6f351d3c360
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-17 20:03:45 +0200

    Improve tapography for better readability on the FAQ Modal

--- app/components/ModalFAQ/Answer.js
+++ app/components/ModalFAQ/Answer.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export default styled.p`
   font-size: 0.9rem;
+  line-height: 1.2rem;
+  font-weight: 400;
   color: #888;
   margin: 0.5rem 0 0;
 `;
--- app/components/ModalFAQ/Question.js
+++ app/components/ModalFAQ/Question.js
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export default styled.h5`
   font-size: 0.9rem;
+  font-weight: 400;
   margin: 0;
 `;
