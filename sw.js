396f7aa2fe3bdfaef7e360f1a6cb712f80ff2d9f
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-21 20:42:00 +0200

    Updates

--- sw.js
+++ sw.js
@@ -57,7 +57,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "4/21/2020, 6:20:46 PM",
+  "version": "4/21/2020, 8:41:57 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": true
