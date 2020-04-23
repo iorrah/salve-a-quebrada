1a4decb51868745b63e2949d02d1509cb6af2cfa
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-23 21:04:51 +0200

    Set fixed height for dynamic images

--- app/components/Store/ImgWrapper.js
+++ app/components/Store/ImgWrapper.js
@@ -5,18 +5,19 @@ const ImgWrapper = styled.div`
   align-items: center;
   background: ${colors.copy};
   display: flex;
-  min-height: auto;
+  height: auto;
+  overflow: hidden;
 
   @media (min-width: 768px) {
-    min-height: 330px;
+    height: 330px;
   }
 
   @media (min-width: 992px) {
-    min-height: 289px;
+    height: 289px;
   }
 
   @media (min-width: 1200px) {
-    min-height: 350px;
+    height: 350px;
   }
 `;
 
