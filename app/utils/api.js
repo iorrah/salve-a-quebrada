6909563c0c0aed144af1874bab743cd39e5849aa
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-23 16:21:49 +0200

    Update api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,11 +1,11 @@
 let API_HOST;
+const environment = process.env.NODE_ENV;
 
-if (process.env.NODE_ENV === 'production') {
+if (environment === 'production') {
   API_HOST = 'https://salve-a-quebrada-api.herokuapp.com';
 } else {
   API_HOST = 'http://localhost:8080';
 }
 
 const API_STORES = `${API_HOST}/v1/stores`;
-
 export { API_STORES };
