a40d09db2a12c947808502aedea30e867dc5d2b0
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-23 18:19:10 +0200

    Improve copy on Q1 and Q4 elements

--- app/components/ModalFAQ/SupporterQ4.js
+++ app/components/ModalFAQ/SupporterQ4.js
@@ -5,14 +5,14 @@ import Link from './Link';
 function SupporterQ4() {
   const answer = (
     <span>
-      Meu nome é Iorrah, eu moro em Berlim e sou apaixonado pelos pequenos
-      cafés, bares e restaurantes locais onde moro. Inspirado pelo projeto{' '}
-      <Link label="SaveOurFaves" href="https://saveourfaves.org/" /> criado em
-      São Francisco nos EUA, eu decidi criar uma versão voltada para
+      Meu nome é Iorrah, eu moro em Berlim e sou apaixonado por bares, butecos,
+      padarias, baladas, cafés e restaurantes locais onde moro. Inspirado pelo
+      projeto <Link label="SaveOurFaves" href="https://saveourfaves.org/" />{' '}
+      criado em São Francisco nos EUA, eu decidi criar uma versão voltada para
       estabelecimentos do Brasil. Uma forma que eu encontrei de ajudar os meus
       lugares favoritos do meu bairro foi comprar vale-presentes pra quando eles
-      abrirem de novo, eu poder voltar a tomar meus cafés favoritos e comer os
-      melhores sanduíches da região.
+      abrirem de novo, eu poder voltar a tomar meus cafés favoritos ou comer o
+      melhor sanduba da região.
     </span>
   );
 
