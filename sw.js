81060e523f04d6d11abc285067400e66e0016414
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-29 18:57:25 +0200

    Updates

--- sw.js
+++ sw.js
@@ -18,8 +18,8 @@ var __wpo = {
       "./1473361f0320627076e31a8c67d0e14b.png",
       "./c8f4c6506e10d724856c0e7e582af193.png",
       "./9d7bcc930ffd1be7448d614f8f737e6d.png",
-      "./runtime.67e4040a2945c1c488de.js",
-      "./main.b82f20ff797223447317.chunk.js.LICENSE.txt",
+      "./runtime.077ce3de1b8b2be182a2.js",
+      "./main.46d5901176de9396d3ff.chunk.js.LICENSE.txt",
       "./"
     ],
     "additional": [
@@ -27,7 +27,7 @@ var __wpo = {
       "./npm.css-loader.8f60f746e9c62fd55421.chunk.js",
       "./npm.redux-saga.49e36373cb2d113df803.chunk.js",
       "./npm.webpack.ad6f4021e78565dadbe7.chunk.js",
-      "./main.b82f20ff797223447317.chunk.js",
+      "./main.46d5901176de9396d3ff.chunk.js",
       "./npm.babel.524524bcea475bf42ad6.chunk.js",
       "./npm.connected-react-router.bf0b95778a4baa99b4c5.chunk.js",
       "./npm.lodash.d1032c6eae05b1256830.chunk.js",
@@ -64,7 +64,7 @@ var __wpo = {
     "fda938959a5bcb6b51135c44d541cd47abc9e910": "./npm.css-loader.8f60f746e9c62fd55421.chunk.js",
     "5eea83f5856b8f77dda17c74bedd31d51d08ff17": "./npm.redux-saga.49e36373cb2d113df803.chunk.js",
     "606bdc668802534e48ef14560ced198a1772d97a": "./npm.webpack.ad6f4021e78565dadbe7.chunk.js",
-    "1d1d636d991674d8d22be305707f50309adb2433": "./main.b82f20ff797223447317.chunk.js",
+    "8a3840e080f86201662d0067e2186509454ed22b": "./main.46d5901176de9396d3ff.chunk.js",
     "1d7ae6d3e6cd5d8eb6f21eb94e28ed23720ce8c7": "./npm.babel.524524bcea475bf42ad6.chunk.js",
     "7ece45f0e1ec5694582d75cc665ef7535e40636f": "./npm.connected-react-router.bf0b95778a4baa99b4c5.chunk.js",
     "baa0ee4cc9732cf3b758f6841f127812237ecb5e": "./npm.lodash.d1032c6eae05b1256830.chunk.js",
@@ -72,16 +72,16 @@ var __wpo = {
     "57e31b205c8fd1a8cad40db118a489b266a1f192": "./npm.react-redux.f9a47373bf5507c04527.chunk.js",
     "e5ec46febf098551eab920589fb5cec3ba47230b": "./npm.react-toastify.96d5c3d033145653ed76.chunk.js",
     "163931f072b62078b0ddd4b04ad31f4e282675cd": "./npm.react-transition-group.f82a127e54d20387c537.chunk.js",
-    "8af1d52df7a74a54301c2355b6a3dfa835d83bd5": "./runtime.67e4040a2945c1c488de.js",
+    "2a5c438bcc363b501b00660b53461ebec9e24a41": "./runtime.077ce3de1b8b2be182a2.js",
     "d95ea640cdfd109a742ea7c6856b039ba1956df7": "./13.fc1b072dbd0fbc1c3bf1.chunk.js",
     "648166fdecbdcb55188c5bb2d54dca995f63e303": "./14.5e15109ea6fcf74acfd0.chunk.js",
     "528421186dfa176d103bd92bfc3a6101a2c8cae1": "./15.9629b3d19d4ab8f2dedb.chunk.js",
-    "a08192864711307478f30b2a169ede7fba97cce0": "./main.b82f20ff797223447317.chunk.js.LICENSE.txt",
-    "1c083883f6489e04ef8373576a6246d98fda8e58": "./"
+    "a08192864711307478f30b2a169ede7fba97cce0": "./main.46d5901176de9396d3ff.chunk.js.LICENSE.txt",
+    "977e556a7de223d3558210a63567c4895649d537": "./"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "4/29/2020, 6:05:40 PM",
+  "version": "4/29/2020, 6:57:23 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": true
@@ -166,7 +166,7 @@ var __wpo = {
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/salve-a-quebrada/";
+/******/ 	__webpack_require__.p = "http://www.salveaquebrada.org/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
