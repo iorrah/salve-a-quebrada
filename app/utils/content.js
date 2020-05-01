6cf1282b25fffe77b35d05f01cbde0d4d4b1bec6
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-05-01 15:19:52 +0200

    Add more CTA suffix options

--- app/utils/content.js
+++ app/utils/content.js
@@ -3,6 +3,30 @@ const getCtaSuffix = url => {
     return 'no Yelp';
   }
 
+  if (url.toLowerCase().indexOf('facebook') >= 0) {
+    return 'no Facebook';
+  }
+
+  if (url.toLowerCase().indexOf('instagram') >= 0) {
+    return 'no Instagram';
+  }
+
+  if (url.toLowerCase().indexOf('patreon') >= 0) {
+    return 'no Patreon';
+  }
+
+  if (url.toLowerCase().indexOf('catarse') >= 0) {
+    return 'no Catarse';
+  }
+
+  if (url.toLowerCase().indexOf('benfeitoria') >= 0) {
+    return 'no Benfeitoria';
+  }
+
+  if (url.toLowerCase().indexOf('apoia.se') >= 0) {
+    return 'no Apoia-se';
+  }
+
   return 'Doações';
 };
 
